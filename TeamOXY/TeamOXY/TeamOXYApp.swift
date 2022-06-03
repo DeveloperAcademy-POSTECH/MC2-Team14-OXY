46e9b1768745a07ceb3c8d5ab4c8e23fc87e259e
Cheong Jaeyun <59136391+imparang@users.noreply.github.com>
2022-06-03 12:42:22 +0900

    feat: Introduce mini challenge 2 project

---
+++ TeamOXY/TeamOXY/TeamOXYApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeamOXYApp.swift
+//  TeamOXY
+//
+//  Created by 정재윤 on 2022/06/03.
+//
+
+import SwiftUI
+
+@main
+struct TeamOXYApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
