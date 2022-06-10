5190716ed6c5a4069cf35d6ccfdc05ff22a5dbd9
Cheong Jaeyun <59136391+imparang@users.noreply.github.com>
2022-06-11 00:17:38 +0900

    Rename: HomeView 삭제되서 다시 추가

---
+++ TeamOXY/TeamOXY/View/HomeView.swift
@@ -0,0 +1,56 @@
+//
+//  HomeView.swift
+//  TeamOXY
+//
+//  Created by 이성수 on 2022/06/09.
+//
+
+import SwiftUI
+
+
+struct HomeView: View {
+    var body: some View {
+        NavigationView{
+            VStack {
+                Spacer()
+                
+                HStack {
+                    Text("누구나\n쉬는시간이\n필요하니까.")
+                        .font(.custom("Pretendard-Light", size: 34))
+                    Spacer()
+                }
+                .padding(.leading, 78)
+                .padding(.bottom)
+                
+                Image("Logo")
+                
+                Spacer()
+                
+                VStack {
+                    Text("팀원들을 초대할 방을 만들어 주세요.")
+                        .font(.custom("Pretendard-SemiBold", size: 12))
+                    
+                    RoundButton(buttonType: .primary, title: "방 만들기") {
+                        print("방만들기")
+                    }
+                }
+                .padding(.bottom)
+                
+                VStack {
+                    Text("이미 방이 있다면 qr코드를 통해 입장해주세요.")
+                        .font(.custom("Pretendard-SemiBold", size: 12))
+                    RoundButton(buttonType: .outline, title: "입장하기") {
+                        print("입장하기")
+                    }
+                }
+            }
+            .navigationBarHidden(true)
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
