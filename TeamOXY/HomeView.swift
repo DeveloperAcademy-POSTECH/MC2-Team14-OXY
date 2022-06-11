5ec381334829caf8b5ec592aa3451f87ba3a84a4
22Seongsoo <103010751+22Seongsoo@users.noreply.github.com>
2022-06-11 17:28:04 +0900

    Feat: [#32] 온보딩 페이지 구성
    온보딩 페이지 구성
    
    탭뷰를 버튼으로 수정하기
    온보딩 이미지, 텍스트 넣기

--- TeamOXY/HomeView.swift
+++ TeamOXY/HomeView.swift
@@ -9,7 +9,19 @@ import SwiftUI
 
 
 struct HomeView: View {
+    @AppStorage("_isFirstLaunching") var isFirstLaunching: Bool = true
+//    온보딩 페이지를 앱 설치 후 최초 실행할때만 띄우는 변수, @AppStorage에 저장되어 앱 종료 후에도 유지시킨다.
+    
     var body: some View {
+        
+        Text("App Main")
+            // 앱 최초 구동 시 전체화면으로 OnboardingTabView 띄우기
+//            .fullScreenCover(isPresented: $isFirstLaunching)
+        
+            .fullScreenCover(isPresented: Binding.constant(true)) {
+                OnboardingView(isFirstLaunching: $isFirstLaunching)
+            }
+        
         NavigationView{
             VStack {
                 Spacer()
