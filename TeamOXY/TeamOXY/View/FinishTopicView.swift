975fb37d5234c64aa6bd48cf5881052434216259
ccdkk <91456952+ccdkk@users.noreply.github.com>
2022-06-12 20:33:11 +0900

    [Feat] 미팅룸 오류수정, 컴플리션로티추가
    - 중복된 텍스트가 겹치면서 캐러셀이 밀리는 현상 수정
    - 니로가 보내준 컴플리션 로티 FinishTopicView에 연결

--- TeamOXY/TeamOXY/View/FinishTopicView.swift
+++ TeamOXY/TeamOXY/View/FinishTopicView.swift
@@ -9,40 +9,58 @@ import SwiftUI
 
 struct FinishTopicView: View {
     
+    @ObservedObject var viewModel: CompletionViewModel
+    
+    
     @Binding var FinishTopicViewCondition: [Bool]
     
     var body: some View {
-        VStack {
-            Text("🕰⏰🕰⏰")
-                .font(.system(size: 58))
-                .padding(.bottom, 20)
-            Text("쉬는 시간을\n 설정하시겠습니까?")
-                .font(.custom("Pretendard-Light", size: 24))
-                .fontWeight(.bold)
-                .multilineTextAlignment(.center)
-                .padding(.bottom, 20)
-            HStack {
-                Button(action: {
-                    FinishTopicViewCondition = []
-                }) {
-                    Circle()
-                        .stroke(Color("PrimaryBlue"))
-                        .frame(width: 58, height: 58, alignment: .center)
-                        .overlay(Text("아니요").font(.custom("Pretendard-Bold", size: 14)).foregroundColor(Color("PrimaryBlue")))
+        ZStack(alignment: .center) {
+            
+            VStack {
+                Text("🕰⏰🕰⏰")
+                    .font(.system(size: 58))
+                    .padding(.bottom, 20)
+                Text("쉬는 시간을\n 설정하시겠습니까?")
+                    .font(.custom("Pretendard-Light", size: 24))
+                    .fontWeight(.bold)
+                    .multilineTextAlignment(.center)
+                    .padding(.bottom, 20)
+                HStack {
+                    Button(action: {
+                        // FinishTopicView뜨는 조건 초기화
+                        FinishTopicViewCondition = []
+                        // 로티 끄기
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
+                            viewModel.isCompletion = true
+                        }
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 1.3) {
+                            viewModel.isCompletion = false
+                        }
+                        
+                        
+                    }) {
+                        Circle()
+                            .stroke(Color("PrimaryBlue"))
+                            .frame(width: 58, height: 58, alignment: .center)
+                            .overlay(Text("아니요").font(.custom("Pretendard-Bold", size: 14)).foregroundColor(Color("PrimaryBlue")))
+                    }
+                    Spacer()
+                    Button(action: {
+                        // FinishTopicView뜨는 조건 초기화
+                        FinishTopicViewCondition = []
+                    }) {
+                        Circle()
+                            .fill(Color("PrimaryBlue"))
+                            .frame(width: 58, height: 58, alignment: .center)
+                            .overlay(Text("네").font(.custom("Pretendard-Bold", size: 14)).foregroundColor(.white))
+                    }
                 }
-                Spacer()
-                Button(action: {
-                    FinishTopicViewCondition = []
-                }) {
-                    Circle()
-                        .fill(Color("PrimaryBlue"))
-                        .frame(width: 58, height: 58, alignment: .center)
-                        .overlay(Text("네").font(.custom("Pretendard-Bold", size: 14)).foregroundColor(.white))
-                }
+                .padding(.horizontal, 40)
             }
-            .padding(.horizontal, 40)
+            .padding(.horizontal, 62)
+            
         }
-        .padding(.horizontal, 62)
     }
 }
 //
