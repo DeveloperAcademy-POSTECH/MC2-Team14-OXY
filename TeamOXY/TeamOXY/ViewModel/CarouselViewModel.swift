a848d6a2306f62d62e7b7499b2727136e6edc775
Cheong Jaeyun <59136391+imparang@users.noreply.github.com>
2022-06-15 20:48:20 +0900

    feat: Create CarouselViewModel 생성

---
+++ TeamOXY/TeamOXY/ViewModel/CarouselViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  CarouselViewModel.swift
+//  TeamOXY
+//
+//  Created by 정재윤 on 2022/06/15.
+//
+
+import SwiftUI
+import Firebase
+import FirebaseFirestore
+import FirebaseFirestoreSwift
+
+class CarouselViewModel: ObservableObject {
+    @Published var topic = ""
+}
