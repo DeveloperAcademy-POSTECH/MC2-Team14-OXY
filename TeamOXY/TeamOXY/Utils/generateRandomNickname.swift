993b884aeaf44c40565480ad22ea15644df85758
Cheong Jaeyun <59136391+imparang@users.noreply.github.com>
2022-06-16 16:21:32 +0900

    Feat: 자동 부여 닉네임 추가

--- TeamOXY/TeamOXY/Utils/generateRandomNickname.swift
+++ TeamOXY/TeamOXY/Utils/generateRandomNickname.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 func generateRandomNickname(_ nickname: String? = nil) -> String {
-    let nicknames: [String] = ["익명의 원숭이", "익명의 코끼리", "익명의 임팔라", "익명의 앵무새", "익명의 호랑이", "익명의 거북이"]
+    let nicknames: [String] = ["익명의 원숭이", "익명의 코끼리", "익명의 임팔라", "익명의 앵무새", "익명의 호랑이", "익명의 거북이", "익명의 기린", "익명의 고양이", "익명의 한우", "익명의 토끼", "익명의 코뿔소", "익명의 비둘기", "익명의 족제비", "익명의 너구리", "익명의 강아지", "익명의 여우", "익명의 늑대"]
     
     guard let nickname = nickname else {
         return nicknames.randomElement() ?? ""
